e186f257542e5f293a7871b15e1eeb09a35c299d
jeffptton <39930443+jeffptton@users.noreply.github.com>
2018-06-29 23:06:59 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionsTest1.Model
+{
+    public class ApplicationDbContext :DbContext
+    {
+        internal object myConnections;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+     
+        }
+        // creqate a DB Set property to add connections to database
+        public DbSet<Connections> ConnectionsItems { get; set; }
+        // **this property is a table in your database 
+    }
+
+}
---
+++ Connections.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionsTest1.Model
+{
+    public class Connections
+    {
+        public int ID { get; set; }
+        public string MovieTitle{ get; set; }
+        public string Actor { get; set; }
+        public int Rating { get; set; }
+
+
+    }
+}
